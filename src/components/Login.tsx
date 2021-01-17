115df9aa5d0e8c6af8377db726ae49b33bb2b6c4
MarcoRy <rydzewski.marek86@gmail.com>
2021-01-17 15:37:40 +0100

    Created required():  - checking if field is not empty

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -3,8 +3,20 @@ import CheckButton from 'react-validation/build/button';
 import Form from 'react-validation/build/form'
 import Input from "react-validation/build/input";
 import isEmail from 'validator';
+
+const required = (value:string) =>{
+    if(!value){
+        return(
+            <div className="alert, alert-danger" role='alert'>
+                This field is required
+            </div>
+        )
+    }
+};
+
+
 const Login = () => {
-    const aaa = "some text";
+
 
 
     return (
