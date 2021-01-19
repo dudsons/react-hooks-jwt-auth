5a89b59b44bfe8fe7dd22254fad5b16947845824
MarcoRy <rydzewski.marek86@gmail.com>
2021-01-19 20:59:16 +0100

    Added Home page

---
+++ src/components/Home.tsx
@@ -0,0 +1,31 @@
+import React, {useEffect, useState} from 'react';
+import UserService from '../services/user.service';
+import {Simulate} from "react-dom/test-utils";
+
+const Home = () => {
+
+    const [content, setContent] = useState<any>();
+
+
+    useEffect(()=>{
+        UserService.getPublicContent().then(
+            (response) => {
+                setContent(response!);
+            },
+            (error) =>{
+                const errMessage = (error.response && error.response.data && error.response.data.message) || error.message || error.toString();
+                setContent(errMessage);
+            }
+        )
+    },[]);
+
+    return (
+        <div className="container">
+            <header className="jumbotron">
+                <h3>{content}</h3>
+            </header>
+        </div>
+    )
+};
+
+export default Home;
